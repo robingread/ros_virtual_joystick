f2f0b2dcb59f8fa4cdf507b7b542a46c78de9eb9
Robin Read <robin.g.read@gmail.com>
2025-05-29 16:57:54 +0100

    Adding classes for Single and Dual pad joystick RViz plugins.

--- include/ros_virtual_joystick/rviz_plugin.hpp
+++ include/ros_virtual_joystick/rviz_plugin.hpp
@@ -7,6 +7,7 @@
 
 namespace ros_virtual_joystick {
 namespace plugins {
+namespace rviz {
 
 /**
  * @brief The RViz Panel plugin, as the name suggests, provides a Panel that can be loaded into RViz and display the
@@ -35,10 +36,33 @@ public slots:
    */
   void onUpdate();
 
+protected:
+  virtual Widget::Config makeConfig() const = 0;
+
 private:
+  QVBoxLayout *layout_;
   std::shared_ptr<Widget> widget_;
   std::unique_ptr<PublisherBase> publisher_;
 };
 
+class SinglePadJoystickPlugin : public RVizPanel {
+protected:
+  Widget::Config makeConfig() const override {
+    Widget::Config cfg;
+    cfg.layout = Layout::SINGLE;
+    return cfg;
+  }
+};
+
+class DualPadJoystickPlugin : public RVizPanel {
+protected:
+  Widget::Config makeConfig() const override {
+    Widget::Config cfg;
+    cfg.layout = Layout::DUAL;
+    return cfg;
+  }
+};
+
+}  // namespace rviz
 }  // namespace plugins
 }  // namespace ros_virtual_joystick
--- src/rviz_plugin.cpp
+++ src/rviz_plugin.cpp
@@ -3,17 +3,16 @@
 
 namespace ros_virtual_joystick {
 namespace plugins {
+namespace rviz {
 
-RVizPanel::RVizPanel(QWidget *parent) : rviz_common::Panel(parent) {
-  const Widget::Config cfg;
-  widget_ = std::make_shared<Widget>(cfg, this);
-
-  QVBoxLayout *layout = new QVBoxLayout(this);
-  layout->addWidget(widget_.get());
-  setLayout(layout);
+RVizPanel::RVizPanel(QWidget *parent) : rviz_common::Panel(parent), layout_(new QVBoxLayout(parent)) {
+  setLayout(layout_);
 }
 
 void RVizPanel::onInitialize() {
+  widget_ = std::make_shared<Widget>(makeConfig(), this);
+  layout_->addWidget(widget_.get());
+
   auto node = getDisplayContext()->getRosNodeAbstraction().lock()->get_raw_node();
   publisher_ = std::make_unique<Publisher>(*node);
 
@@ -25,9 +24,11 @@ void RVizPanel::onUpdate() {
   publisher_->publishState(state);
 }
 
+}  // namespace rviz
 }  // namespace plugins
 }  // namespace ros_virtual_joystick
 
 // This macro registers the plugin
 #include <pluginlib/class_list_macros.hpp>
-PLUGINLIB_EXPORT_CLASS(ros_virtual_joystick::plugins::RVizPanel, rviz_common::Panel)
+PLUGINLIB_EXPORT_CLASS(ros_virtual_joystick::plugins::rviz::SinglePadJoystickPlugin, rviz_common::Panel);
+PLUGINLIB_EXPORT_CLASS(ros_virtual_joystick::plugins::rviz::DualPadJoystickPlugin, rviz_common::Panel);
