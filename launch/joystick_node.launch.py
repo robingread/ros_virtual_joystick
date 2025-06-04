67be2773a2c3bc04bfcb6e356a3b36109ae37e73
Robin Read <robin.g.read@gmail.com>
2025-06-04 12:17:01 +0100

    Updating the joystick_node.launch.py file to accept the num_pads command line argument and pass it to the standalone node.

--- launch/joystick_node.launch.py
+++ launch/joystick_node.launch.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python3
 
-import os
-
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description() -> LaunchDescription:
+    num_pads = LaunchConfiguration("num_pads")
     return LaunchDescription(
         [
+            DeclareLaunchArgument(
+                "num_pads",
+                default_value="2",
+                description="Number of pads use. Acceptable values are [1, 2].",
+                choices=["1", "2"],
+            ),
             Node(
                 package="ros_virtual_joystick",
                 executable="ros_virtual_joystick_node",
                 name="ros_virtual_joystick",
                 output="screen",
+                parameters=[{"num_pads": num_pads}],
             ),
         ]
     )
