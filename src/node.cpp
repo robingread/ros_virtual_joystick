e31f8474b24a165a5306024d7f2bfa07a5c5181f
Robin Read <robin.g.read@gmail.com>
2025-06-05 15:01:24 +0100

    Updating the node.cpp file so that a topic can be set dynamically when the node is started by using ROS params.

--- src/node.cpp
+++ src/node.cpp
@@ -11,15 +11,16 @@ int main(int argc, char **argv) {
 
   // Create a ROS node and spin in the background
   auto node = std::make_shared<rclcpp::Node>("joystick_gui_node");
+  const std::string joy_topic = node->declare_parameter("joy_topic", "joy");
   const int count = node->declare_parameter("num_pads", 2);
   const ros_virtual_joystick::Layout layout = static_cast<ros_virtual_joystick::Layout>(count - 1);
 
   std::thread ros_spin_thread([node]() { rclcpp::spin(node); });
 
   // Create the publisher and the widget
-  auto publisher = std::make_unique<ros_virtual_joystick::Publisher>(*node);
+  auto publisher = std::make_unique<ros_virtual_joystick::Publisher>(*node, joy_topic);
 
-  const ros_virtual_joystick::Widget::Config cfg{250, layout};
+  const ros_virtual_joystick::Widget::Config cfg{250, layout, joy_topic};
   ros_virtual_joystick::Widget widget(cfg);
   widget.show();
 
@@ -27,6 +28,10 @@ int main(int argc, char **argv) {
   QObject::connect(&widget, &ros_virtual_joystick::Widget::stateUpdated, [&publisher, &widget]() {
     const auto state = widget.getState();
     publisher->publishState(state);
+  });
+
+  QObject::connect(&widget, &ros_virtual_joystick::Widget::topicUpdated, [&publisher](const QString &topic) {
+    publisher->reset(topic.toStdString());
   });
 
   // Add a QTimer that periodically checks for shutdown
